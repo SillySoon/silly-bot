da8b59c646e287c1cdb36cd8088ee47b7a894194
SillySoon <silly@sillysoon.de>
2024-02-02 20:42:53 +0100

    Added Activity

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { commands } from "./commands";
 import { deployCommands } from "./deploy-commands";
 import { config } from "./utils/config";
 import { db } from "./utils/db";
+import { version } from "../package.json";
 import logger from "silly-logger";
 
 // Setup logger
@@ -13,7 +14,7 @@ logger.logFolderPath("./logs");
 
 // Start up a client
 export const client = new Client({
-	intents: ["Guilds", "GuildMessages", "DirectMessages"],
+  intents: ["Guilds", "GuildMessages", "DirectMessages"],
 });
 
 // Setup database
@@ -21,49 +22,58 @@ db.setup();
 
 // Run command when interaction is created
 client.on("interactionCreate", async (interaction) => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
-	if (commands[commandName as keyof typeof commands]) {
-		try {
-			commands[commandName as keyof typeof commands].execute(interaction);
-			logger.custom(
-				"INTERACTION",
-				"#ffffff",
-				"",
-				`By ${interaction.member?.user.username} - /${interaction.commandName}`
-			);
-		} catch (error: any) {
-			logger.error(error);
-			try {
-				await interaction.reply({
-					content: `There was an error while executing this command!\nJoin the Support Server and send us the error message:\n \`\`\`js\n ${JSON.stringify(
-						interaction,
-						null,
-						4
-					)} \`\`\` \`\`\`js\n${error}\`\`\` \n (DISCORD INVITE) `,
-					embeds: [],
-					ephemeral: true,
-				});
-			} catch (err: any) {
-				logger.error(err);
-				await interaction.reply({
-					content: `There was an error while executing this command, but there was another error sending the error message!\n Please contact us and include the following information: \`command name, options, time of execution\` \n (DISCORD INVITE) `,
-					embeds: [],
-					ephemeral: true,
-				});
-			}
-		}
-	}
+  const { commandName } = interaction;
+  if (commands[commandName as keyof typeof commands]) {
+    try {
+      commands[commandName as keyof typeof commands].execute(interaction);
+      logger.custom(
+        "INTERACTION",
+        "#ffffff",
+        "",
+        `By ${interaction.member?.user.username} - /${interaction.commandName}`
+      );
+    } catch (error: any) {
+      logger.error(error);
+      try {
+        await interaction.reply({
+          content: `There was an error while executing this command!\nJoin the Support Server and send us the error message:\n \`\`\`js\n ${JSON.stringify(
+            interaction,
+            null,
+            4
+          )} \`\`\` \`\`\`js\n${error}\`\`\` \n (DISCORD INVITE) `,
+          embeds: [],
+          ephemeral: true,
+        });
+      } catch (err: any) {
+        logger.error(err);
+        await interaction.reply({
+          content: `There was an error while executing this command, but there was another error sending the error message!\n Please contact us and include the following information: \`command name, options, time of execution\` \n (DISCORD INVITE) `,
+          embeds: [],
+          ephemeral: true,
+        });
+      }
+    }
+  }
 });
 
 // Console log when bot is ready
 client.once("ready", () => {
-	if (!client.user) return;
-	logger.success(`Ready! Logged in as ${client.user.tag}`);
+  if (!client.user) return;
+  logger.success(`Ready! Logged in as ${client.user.tag}`);
 
-	// Deploy all commands
-	deployCommands();
+  // Deploy all commands
+  deployCommands();
+
+  // Interval to refresh activity
+  setInterval(async () => {
+    if (!client.user) return;
+    client.user.setActivity(`v${version}`, { type: ActivityType.Watching });
+  }, 15000);
+
+  // Kickstart activity
+  client.user.setActivity(`v${version}`, { type: ActivityType.Watching });
 });
 
 client.login(config.DISCORD_TOKEN);
